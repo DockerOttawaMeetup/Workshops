a2a44c3ea0365224b9e278c27015406459a81917
Andres Torres <andres.torres@qlik.com>
2016-05-25 14:08:01 -0400

    from node 6

--- 2016-05-25/node-movies/server.js
+++ 2016-05-25/node-movies/server.js
@@ -1,20 +1,44 @@
 var mongoose = require('mongoose')
+var express = require('express')
+var app = express()
 
 var db = mongoose.connection
 
 db.on('error', console.error)
-db.once('open', function() {
-    console.log('Connected to mongo')
-});
-
-mongoose.connect('mongodb://mongo/test')
+db.once('open', function () {
+  console.log('Connected to mongo')
+})
 
 var movieSchema = mongoose.Schema({
-    title: String
+  title: String
 })
 var Movie = mongoose.model('Movie', movieSchema)
 
 var movie1 = new Movie({ title: 'The Princess Bride' })
 movie1.save(function (err, movie) {
-    if (err) return console.error(err)
+  if (err) {
+    return console.error(err)
+  }
+})
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/movies', function (req, res) {
+  Movie.find({}, (err, movies) => {
+    if (err) {
+      res.send(500, err)
+    }
+    res.send(movies)
+  })
+})
+
+mongoose.connect('mongodb://mongo/test', function (err) {
+  if (err) {
+    process.exit()
+  }
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+  })
 })
